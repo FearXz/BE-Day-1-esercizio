175de7503f52bcaf3b2c8ad40adaf90026ff9952
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2024-01-29 14:36:39 +0100

    aggiunte tutte le classi

--- esercizio-1-classi/Program.cs
+++ esercizio-1-classi/Program.cs
@@ -1,12 +1,18 @@
 ﻿
-namespace Esercizio1  {
+namespace Esercizio1 {
     class Program
     {
-        static void Main(string[] args) 
+        static void Main(string[] args)
         {
 
             Atleta atleta1 = new Atleta("Pippo", "Franco", 32, "Strongman");
-           atleta1.showAtleta();
+            atleta1.showAtleta();
+            Dipendente dipendente1 = new Dipendente("paperino", "pluto", 22, "logistica");
+            dipendente1.showDipendente();
+            Animale animale1 = new Animale("Rettile", "Coccodrillo", 8);
+            animale1.showAnimale();
+            Veicolo veicolo1 = new Veicolo("Lamborhini","xxxAntoxxx");
+            veicolo1.showVeicolo();
         }
     }
     class Atleta
@@ -16,33 +22,36 @@ namespace Esercizio1  {
         public int eta;
         public string specializzazione;
 
-        public Atleta (string nome, string cognome, int eta, string specializzazione)
+        public Atleta(string nome, string cognome, int eta, string specializzazione)
         {
             this.nome = nome;
             this.cognome = cognome;
             this.eta = eta;
             this.specializzazione = specializzazione;
         }
-        public void showAtleta ()
+        public void showAtleta()
         {
             Console.WriteLine($"l'atleta 1 è : {this.nome} {this.cognome}, ed è uno specilista in {this.specializzazione}");
         }
 
     }
-
     class Dipendente
     {
         public string nome;
         public string cognome;
         public int eta;
         public string reparto;
-       
-        public Dipendente (string nome, string cognome ,  int eta, string reparto)
+
+        public Dipendente(string nome, string cognome, int eta, string reparto)
         {
-            this.nome = nome;  
+            this.nome = nome;
             this.cognome = cognome;
             this.eta = eta;
-            this.reparto = reparto; 
+            this.reparto = reparto;
+        }
+        public void showDipendente()
+        {
+            Console.WriteLine($"il dipendente 1 è : {this.nome} {this.cognome}, e lavora nel reparto {this.reparto}");
         }
     }
     class Animale
@@ -56,5 +65,26 @@ namespace Esercizio1  {
             this.razza = razza;
             this.eta = eta;
         }
+        public void showAnimale()
+        {
+            Console.WriteLine($"l'animale è un : {this.specie} razza: {this.razza}, ed ha {this.eta} anni");
+        }
+
+
+    }
+    class Veicolo {
+        public string modello;
+        public string targa;
+        public Veicolo(string modello, string targa)
+        {
+            this.modello = modello;
+            this.targa = targa;
+        }
+    
+    public void showVeicolo()
+    {
+        Console.WriteLine($"modello auto : {this.modello} targa : {this.targa}");
+    }
     }
 }
+
